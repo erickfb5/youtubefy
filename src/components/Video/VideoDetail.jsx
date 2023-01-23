17d08a9518a948cf0f48d82b6308a9a8d9673115
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-23 13:02:43 -0300

    updating VideoDetails

--- src/components/Video/VideoDetail.jsx
+++ src/components/Video/VideoDetail.jsx
@@ -18,19 +18,39 @@ const VideoDetail = () => {
     );
   }, [id]);
 
+  if (!videoDetail?.snippet) return `Loading...`;
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
+
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
             <ReactPlayer
-            className='react-player'
-            url={`https://www.youtube.com/watch?v=${id}`} 
-            controls 
+              className="react-player"
+              url={`https://www.youtube.com/watch?v=${id}`}
+              controls
             />
-          <Typography>
-            {videoDetail.snippet.title}
-          </Typography>
+            <Typography variant="h5" color="#fff" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: "#fff" }}
+              px={2}
+              py={1}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant={{ sm: "subtitle1", md: "h6" }}>
+                  {channelTitle}
+                </Typography>
+              </Link>
+            </Stack>
           </Box>
         </Box>
       </Stack>
