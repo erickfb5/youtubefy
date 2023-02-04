b0da4d96951fc140b04a6ddf9fa048361debd93a
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-02-03 23:54:30 -0300

    updated componentes & fixed bugs

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,4 +1,3 @@
-export {default as VideoBox} from './VideoBox'
 export {default as VideoCard} from './VideoCard'
 export {default as VideoDetail} from './VideoDetail'
 export {default as Videos} from './Videos'
